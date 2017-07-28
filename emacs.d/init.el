e1de7125fb69d9d4a70e6abf70aeca13e968ae27
rrudakov <rrudakov@expertoption.com>
2017-07-28 18:41:08 +0300

    Move to ivy and Hack as default font

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -48,14 +48,14 @@
      ("\\.idl\\'" flymake-simple-make-init nil nil))))
  '(package-selected-packages
    (quote
-    (company-shell org-bullets gitignore-mode magit company-quickhelp virtualenvwrapper epc json-mode gruvbox-theme ido-vertical-mode popwin winum spaceline diminish yaml-mode company-c-headers company-cabal company-go company-jedi solaire-mode highlight-symbol diff-hl anzu paradox rainbow-delimiters tox ini-mode window-numbering use-package web-mode spaceline-all-the-icons smartparens projectile org-alert org nlinum neotree intero flycheck-color-mode-line flx-ido doom-themes dired+ company-web)))
- '(paradox-automatically-star t)
+    (ivy-todo counsel-projectile all-the-icons-ivy counsel swiper htmlize w3m org-mime company-shell org-bullets gitignore-mode magit company-quickhelp virtualenvwrapper epc json-mode gruvbox-theme ido-vertical-mode popwin winum spaceline diminish yaml-mode company-c-headers company-cabal company-go company-jedi solaire-mode highlight-symbol diff-hl anzu paradox rainbow-delimiters tox ini-mode window-numbering use-package web-mode spaceline-all-the-icons smartparens projectile org-alert org nlinum neotree intero flycheck-color-mode-line flx-ido doom-themes dired+ company-web)))
  '(paradox-github-token t)
  '(safe-local-variable-values
    (quote
     ((eval venv-workon "at-env")
      (haskell-process-use-ghci . t)
-     (haskell-indent-spaces . 4)))))
+     (haskell-indent-spaces . 4))))
+ '(send-mail-function (quote smtpmail-send-it)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 ############### Variables:
 
-dir=~/dotfiles
+dir=${HOME}/dotfiles
 files="Xresources emacs.d/init.el tmux.conf vimrc xmonad/xmonad.hs xmobarrc xsession gitconfig stalonetrayrc zshrc zshenv aliasrc Xmodmap"
 
 ###############
@@ -16,12 +16,12 @@ files="Xresources emacs.d/init.el tmux.conf vimrc xmonad/xmonad.hs xmobarrc xses
 # Changing to the dotfiles directory
 
 echo "Changing to the $dir directory"
-cd $dir
+cd ${dir}
 echo "...done"
 
 # Move any existing dotfiles to ~/dotfiles_old, then creating symlinks
 
 for file in $files; do
     echo "Creating symlinks for $file in home directory"
-    ln -sf $dir/$file ~/.$file
+    ln -sf ${dir}/${file} ${HOME}/.${file}
 done
