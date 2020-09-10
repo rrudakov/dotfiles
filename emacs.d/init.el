cab3f0d8104285410ce66a00fc5bf57e2dbef155
Roman Rudakov <rrudakov@protonmail.com>
2020-09-10 17:10:35 +0200

    Emacs config: Reorganize and add descriptions (Part 5)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -16,18 +16,9 @@ which is unsafe because it allows man-in-the-middle attacks.
 There are two things you can do about this warning:
 1. Install an Emacs version that does support SSL and be safe.
 2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
   (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
-
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
-(add-to-list
- 'package-archives
- '("emacswiki" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/emacswiki/") t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+  (add-to-list 'package-archives '("emacswiki" . "https://mirrors.tuna.tsinghua.edu.cn/elpa/emacswiki/") t))
 
 (setq gc-cons-threshold (* 64 1024 1024)
       gc-cons-percentage 0.1)
